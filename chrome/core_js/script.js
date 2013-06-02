5ee27c297f199bafdcd9b450caad3203b512d620
Mark Chang <mark23456@hotmail.com>
2013-06-02 23:34:50 +0800

    add chrome code and fix but 'no-exam' in util.js:456

---
+++ chrome/core_js/script.js
@@ -0,0 +1,38 @@
+$('body').prepend('<div id="toppanel"></div>');
+
+$.ajax({
+			type:"GET",
+			url:chrome.extension.getURL("template.html")
+		}).done(function(template) {
+
+			$('#toppanel').prepend(template); 
+			bindOpinonClick();
+			loadButtonImages();
+			autoCompleteSelection();
+			bindCheckBoxForHomework();
+
+			$("#submitButton").bind('click',function(){submit();});
+			$("#privacy").bind('click',function(){ showMessageDialog();});
+			$("input[name=ans1]")[0].click();
+			$("input[name=ans2]")[0].click();
+			$("input[name=ans3]")[0].click();
+			$("input[name=ans4]")[1].click();
+
+			current_course = getCourse();
+			var code = current_course.user;
+
+			var json_id_courses = JSON.stringify( {"authCode": CryptoJS.SHA1(code).toString()} );
+			$.ajax({
+				   type:"POST",
+				   url:"http://r444b.ee.ntu.edu.tw/course_rater/checkUsed.php",
+				   datatype: "json",                                                                                                                                                                                      
+	    		   error: function() { console.log('Uh Oh!'); },
+				   data: {"CHECK": json_id_courses }
+				}).done(function(used) {
+					console.log(used);
+					if ( used == 0 ){
+						$("#privacy").click();
+					}
+			});
+
+		});	
